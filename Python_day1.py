9dc2f1bfcebac3f649aa3538206d8644fa6ca707
Reddonut-cyber <akhowsuk@gmail.com>
2025-01-13 11:21:15 +0700

    Circular

--- Python_day1.py
+++ Python_day1.py
@@ -1,2 +1,17 @@
-x = 5
-sum = x -1
+class Circular :
+    def __init__(self, radius, x, y):
+        self.radius = radius
+        self.x = x
+        self.y = y 
+        
+    def draw(self):
+        print(f"{self.x}, {self.y}")
+        
+    def Move(self, radius, x, y):
+        self.radius = radius
+        self.x = x + 1
+        self.y = y + 1
+
+if __name__ == "__main__":
+    c = Circular(10, 100, 100)
+    c.draw()
