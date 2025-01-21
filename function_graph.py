b6a5c52275d156fd0dd84e03044a536394abd90b
Reddonut-cyber <akhowsuk@gmail.com>
2025-01-21 18:03:21 +0700

    y = x^2

--- function_graph.py
+++ function_graph.py
@@ -9,21 +9,21 @@ logger = logging.getLogger()
 logger.info("Program started")
 
 def f(x):
-    return x
+    return x ** 2
 
 pygame.init()
 
-w = 600
-h = 600
-N = 300
+w = 800
+h = 800
+x_max = 2
+x_min = -2
+scale = 30
+step = 0.01
 
 screen = pygame.display.set_mode((w, h))
 pygame.display.set_caption("Draw graph")
 
 running = True
-
-prev_x = None
-prev_y = None
 
 while running:
     for event in pygame.event.get():
@@ -32,17 +32,23 @@ while running:
             break
 
     screen.fill((0, 0, 0))
-
-    for x in range(-N, N):
-        scaled_x = w // 2 + x 
-        scaled_y = h // 2 - f(x) 
+    
+    prev_x = None
+    prev_y = None
+    x = x_min
+    while x <= x_max:
+        scaled_x = int(w / 2) + int(x * scale) 
+        scaled_y = int(h / 2) - int(f(x) * scale)
         if prev_x is not None and prev_y is not None:
             pygame.draw.line(screen, (255, 255, 255), (prev_x , prev_y), (scaled_x, scaled_y))
             
+            
         prev_x, prev_y = scaled_x, scaled_y
+        x += step
         #screen.set_at((p_x), (255, 255, 255))
         # pygame.draw.line(screen, (0, 255, 0), (10, 10), (300, 300))
 
     pygame.display.flip()
 
 pygame.quit()
+logger.info("Program terminated")
